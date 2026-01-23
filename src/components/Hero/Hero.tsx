550101ff86d4afb8c5c70b67eebbf467c5dbd43e
Alvitaa <juanjoalva19@gmail.com>
2026-01-22 20:14:46 -0500

    added hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,14 @@
 function Hero() {
     return (
-        <>
-            <h3>Hola! Mi nombre es</h3>
-            <h1>Juan Alva</h1>
-            <p>Soy un desarrollador Frontend con experiencia en el desarrollo de aplicaciones Fullstack</p>
-            <p>Construyendo interfaces que conectan usuarios con sistemas</p>
-            <p>Esto es un WIP (Work in progress)</p>
-        </>
+        <div className="w-full h-[calc(100vh-2.5rem)] place-items-center content-center">
+            <div className="border-8 py-35 px-20 bg-white leading-10">
+                <h3>Hola! Mi nombre es</h3>
+                <h1 className="text-6xl font-bold">{"</ Juan Alva>"}</h1>
+                <p>Soy un desarrollador Frontend con experiencia en el desarrollo de aplicaciones Fullstack</p>
+                <p>Construyendo interfaces que conectan usuarios con sistemas</p>
+                <p>Esto es un WIP (Work in progress)</p>
+            </div>
+        </div>
     )
 }
 
