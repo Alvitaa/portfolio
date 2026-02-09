8d8dacb69208056bc055a342275805469344c0e7
Alvitaa <juanjoalva19@gmail.com>
2026-02-09 17:52:24 -0500

    added peru svg and nav tweaks

---
+++ src/assets/icons/Peru.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Peru() {
+    return <>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="20">
+            <path fill="#D91023" d="m0,0h30v20H0" />
+            <path fill="#FFF" d="m10,0h10v20H10" />
+        </svg>
+    </>
+};
+
+export default Peru;
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,35 +1,38 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react";
+import Peru from "../../assets/icons/Peru";
 
-function Clock() {
-    const [time, setTime] = useState("00:00:00");
+const Clock = () => {
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        setInterval(() => {
-            const date = new Date()
-            const offset = date.getTimezoneOffset();
+        const timerID = setInterval(() => { setDate(new Date()) }, 1000);
+        return () => clearInterval(timerID);
+    }, []);
 
-            const hour = date.getHours() + (offset / 60) - 5;
-            const minute = date.getMinutes()
-            const second = date.getSeconds()
+    const timeString = useMemo(() => date.toLocaleTimeString("en-US", {
+        timeZone: "America/Lima", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false
+    }), [date]);
 
-
-            const currentTime = hour + ':' + minute + ':' + second
-            setTime(currentTime)
-        }, 1000)
-    }, [])
-
-    return <div className="font-bold">{time} PE</div>
-}
+    return <span className="w-18 text-right font-bold">{timeString}</span>;
+};
 
 function Nav() {
     return (
-        <nav className="w-full h-10 font-bold border-b-1 flex place-content-around items-center">
-            <a href="#hero">inicio</a>
-            <a href="#about">sobre mi</a>
-            <a href="#projects">proyectos</a>
-            <a href="#skills">habilidades</a>
-            <Clock />
-            <div className="bg-main text-white px-8 py-2">
+        <nav className="w-full gap-4 h-10 font-bold border-b flex place-content-between items-center">
+            <div className="flex flex-row place-content-evenly flex-1">
+                <a href="#hero">inicio</a>
+                <a href="#about">sobre mi</a>
+                <a href="#projects">proyectos</a>
+                <a href="#skills">habilidades</a>
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+                <Clock />
+                <div className="border-3">
+                    <Peru />
+                </div>
+                <span className="font-bold">PE</span>
+            </div>
+            <div className="w-1/9 bg-main text-white h-full py-2 text-center">
                 <h3>EN | ES</h3>
             </div>
         </nav>
