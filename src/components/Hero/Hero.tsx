5b07a2134068d5607e9edde67ac117632a9e7fbf
Alvitaa <juanjoalva19@gmail.com>
2026-02-12 16:12:52 -0500

    translated hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,15 +1,18 @@
+import { useContext } from "react";
 import Backgound from "../../assets/images/cloudy-sky.webp";
+import { LanguageContext } from "../../i18n/LanguageContext";
 
 function Hero() {
+    const {t} = useContext(LanguageContext);
     const steps = 12;
 
     return (
         <section id="hero" className="w-full flex flex-col">
             <div className="-mt-10 py-45 leading-10 text-center bg-cover" style={{backgroundImage: `url(${Backgound})`}}>
-                <h3>Hola! Mi nombre es</h3>
+                <h3>{t("presentation")}</h3>
                 <h1 className="text-6xl font-bold leading-25">Juan José Alva Larios</h1>
-                <p>Soy un desarrollador Frontend con experiencia en el desarrollo de aplicaciones Fullstack</p>
-                <p>"Construyendo interfaces que conectan usuarios con sistemas."</p>
+                <p>{t("profession")}</p>
+                <p>"{t("tagline")}"</p>
             </div>
             {Array.from({ length: steps}).map((_, index) => (
                 <div key={index} className="w-full border-t-5 bg-gray-700 h-10"></div>
--- src/i18n/translations/home.tsx
+++ src/i18n/translations/home.tsx
@@ -1,12 +1,18 @@
 export const home = {
-  en: {
+  es: {
     hero: "inicio",
     about: "sobre mi",
     projects: "proyectos",
+    presentation: "Hola! Mi nombre es",
+    profession: "Soy un desarrollador Frontend con experiencia en el desarrollo de aplicaciones Fullstack",
+    tagline: "Construyendo interfaces que conectan usuarios con sistemas.",
   },
-  es: {
+  en: {
     hero: "home",
     about: "about",
     projects: "projects",
+    presentation: "Hi! My name is",
+    profession: "I'm a Frontend developer with experience in fullstack apps development",
+    tagline: "Building interfaces that connect users with systems",
   },
 };
