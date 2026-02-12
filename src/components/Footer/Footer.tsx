9bc5ba1694f22dc9d83398ef5a216c3afeae8992
Alvitaa <juanjoalva19@gmail.com>
2026-02-11 23:58:26 -0500

    added links and visual tweaks to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,15 @@
 function Footer () {
-    return <footer className="bg-black text-white text-md">
-        <div className="border-l-3 border-white">
-            <h3>Tristemente, este es el final :(</h3>
+    return <footer className="bg-black text-white text-md p-5 flex flex-col place-content-between h-[35vh]">
+        <div className="border-l-3 pl-5 border-white leading-10">
+            <h3 className="text-3xl font-semibold">Tristemente, este es el final :(</h3>
             <p>Pero puedes chequear mis redes sociales, puedes enviarme un mensaje si quieres contactarme!</p>
             <ul>
-                <li><a href="" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                <li><a href="" target="_blank" rel="noopener noreferrer">Github</a></li>
-                <li>Email</li>
+                <li className="underline"><a href="https://www.linkedin.com/in/jalvalarios/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                <li className="underline"><a href="https://github.com/Alvitaa" target="_blank" rel="noopener noreferrer">Github</a></li>
+                <li className="underline">Email</li>
             </ul>
         </div>
-        <div>
+        <div className="text-center font-semibold">
             <p>Hecho por Juan Alva - 2026</p>
             <p>Este sitio web utiliza React + Vite y Tailwind CSS</p>
         </div>
