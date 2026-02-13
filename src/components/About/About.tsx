d1602eb9ecdb4f41dafdf167c7d768eec604e300
Alvitaa <juanjoalva19@gmail.com>
2026-02-13 12:30:45 -0500

    translated correctly hero, about me and footer

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,19 +1,23 @@
+import { useTranslation } from "react-i18next";
+
 function About() {
+    const {t} = useTranslation("home");
+
     return <section className="min-h-[85vh] flex flex-row items-center place-content-center">
         <div className="bg-main flex flex-col place-content-between gap-5 p-5 text-white shadow-2xl min-h-[75vh] min-w-[50vw] max-w-[60vw]">
-            <h3 className="text-5xl font-bold border-b pb-5">Sobre mi</h3>
-            <div className="flex flex-col gap-3">
+            <h3 className="text-5xl font-bold border-b pb-5">{t("about.title")}</h3>
+            <div className="flex flex-col gap-4">
                 <p className="text-lg">
-                    Desde siempre me ha gustado el software y entender el cómo funcionan diversos sistemas detrás de las cosas que usamos a diario, lo que llevó a formarme como bachiller en Ingeniería de Sistemas.
+                    {t("about.text.0")}
                 </p>
                 <p className="text-lg">
-                    Actualmente busco desarrollarme profesionalmente como frontend, fortaleciendo mis habilidades técnicas y ganando experiencia participando en proyectos de impacto.
+                    {t("about.text.1")}
                 </p>
                 <p className="text-lg">
-                    Este portafolio está inspirado en los sistemas de líneas de trenes y sus estaciones, que me resultan fascinantes por su lógica, organización y eficiencia, cualidades que considero fundamentales en el desarrollo de buen software.
+                    {t("about.text.2")}
                 </p>
             </div>
-            <p className="border-t pt-5">Escrito por: Juan Alva - Desarrollador de Software</p>
+            <p className="border-t pt-5 text-right">{t("about.signature", {name: "Juan Alva"})}</p>
         </div>
     </section>
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,10 +8,10 @@ function Hero() {
     return (
         <section id="hero" className="w-full flex flex-col">
             <div className="-mt-10 py-45 leading-10 text-center bg-cover" style={{backgroundImage: `url(${Backgound})`}}>
-                <h3>{t("presentation")}</h3>
+                <h3>{t("hero.presentation")}</h3>
                 <h1 className="text-6xl font-bold leading-25">Juan José Alva Larios</h1>
-                <p>{t("profession")}</p>
-                <p>"{t("tagline")}"</p>
+                <p>{t("hero.profession")}</p>
+                <p>"{t("hero.tagline")}"</p>
             </div>
             {Array.from({ length: steps}).map((_, index) => (
                 <div key={index} className="w-full border-t-5 bg-gray-700 h-10"></div>
