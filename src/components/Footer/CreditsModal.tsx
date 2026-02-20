65a163109e9e2307a090df4d898912a422c4ce6b
Alvitaa <juanjoalva19@gmail.com>
2026-02-20 10:36:32 -0500

    translation of credits

--- src/components/Footer/CreditsModal.tsx
+++ src/components/Footer/CreditsModal.tsx
@@ -1,18 +1,24 @@
+import { useTranslation } from "react-i18next";
+
 function CreditsModal() {
+    const {t} = useTranslation("credits");
+
     return (
         <div className="bg-neutral-200 w-1/2 rounded-xl text-center z-1">
-            <h1 className="bg-main text-white rounded-t-xl p-3 text-3xl font-bold leading-15">Credits</h1>
+            <h1 className="bg-main text-white rounded-t-xl p-3 text-3xl font-bold leading-15">
+                {t("title")}
+            </h1>
             <div className="p-5">
-                <p>Developer - Juan Alva</p>
-                <h3 className="text-2xl font-bold leading-15">Fonts</h3>
-                <p>Inter - <a href="https://rsms.me/inter/">Rasmus Andersson</a></p>
-                <h3 className="text-2xl font-bold leading-15">Images and Icons</h3>
-                <p>Sky image - <a href="https://www.pexels.com/es-es/foto/casa-pintoresca-frente-al-mar-con-exuberante-vegetacion-32918344/">Dr. Mohammad</a></p>
-                <p>Stair render - Juan Alva</p>
-                <p>Gravel image - <a href="https://www.freepik.com/free-photo/vertical-closeup-small-stones-sunlight-nice-picture-backgrounds-wallpapers_10835353.htm#fromView=keyword&page=1&position=10&uuid=e5f03266-8480-4d25-b1cb-eba094fb5fb0&query=Stone+gravel+texture">Wirestock</a> </p>
-                <p>Flag of Peru SVG - <a href="https://en.wikipedia.org/wiki/File:Flag_of_Peru.svg">Wikipedia</a></p>
-                <p>Overwatch SVG - <a href="https://overwatch.blizzard.com/">Blizzard Entertainment</a></p>
-                <h3 className="text-2xl font-bold leading-15">Additional Dependencies</h3>
+                <p>{t("developer")} - Juan Alva</p>
+                <h3 className="text-2xl font-bold leading-15">{t("fonts")}</h3>
+                <p>Inter - <a className="underline" href="https://rsms.me/inter/">Rasmus Andersson</a></p>
+                <h3 className="text-2xl font-bold leading-15">{t("images.title")}</h3>
+                <p>{t("images.sky")} - <a className="underline" href="https://www.pexels.com/es-es/foto/casa-pintoresca-frente-al-mar-con-exuberante-vegetacion-32918344/">Dr. Mohammad</a></p>
+                <p>{t("images.stair")} - Juan Alva</p>
+                <p>{t("images.gravel")} - <a className="underline" href="https://www.freepik.com/free-photo/vertical-closeup-small-stones-sunlight-nice-picture-backgrounds-wallpapers_10835353.htm#fromView=keyword&page=1&position=10&uuid=e5f03266-8480-4d25-b1cb-eba094fb5fb0&query=Stone+gravel+texture">Wirestock</a> </p>
+                <p>{t("images.peru")} - <a className="underline" href="https://en.wikipedia.org/wiki/File:Flag_of_Peru.svg">Wikipedia</a></p>
+                <p>{t("images.ow")} - <a className="underline" href="https://overwatch.blizzard.com/">Blizzard Entertainment</a></p>
+                <h3 className="text-2xl font-bold leading-15">{t("additional")}</h3>
                 <p>React Icons</p>
             </div>
         </div>
