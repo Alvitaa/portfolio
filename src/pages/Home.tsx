e8840c02a58fe75b5f1031273160f183c3b2a616
Alvitaa <juanjoalva19@gmail.com>
2026-02-25 11:46:58 -0500

    added icon and translation for tab title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import About from "../components/About/About";
 import Announcement from "../components/Announcement/Announcement";
 import Beam from "../components/Beam/Beam";
@@ -6,8 +7,15 @@ import PreFooter from "../components/Footer/PreFooter";
 import Hero from "../components/Hero/Hero";
 import Nav from "../components/Nav/Nav";
 import ProjectsPreview from "../components/Projects/ProjectsPreview";
+import { useEffect } from "react";
 
-function Home () {
+function Home() {
+  const { t } = useTranslation("home");
+
+  useEffect(() => {
+    document.title = t("title", {name: "Juan Alva"});
+  }, [t]);
+
   return (
     <>
       <Beam />
